e09c10be67dc860fd2635124513d521bbcdab330
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2025-02-13 11:45:29 +0100

    CaseCBR acabado

--- SnakeProject/Assets/Scripts/CaseCBR.cs
+++ SnakeProject/Assets/Scripts/CaseCBR.cs
@@ -26,12 +26,15 @@ public class CaseCBR
     private Dictionary<string,bool> similarityBool;
     private Dictionary<string,List<bool>> similarityBoolList;
 
+    private dynamic answer; // Es dinamico porque no sabemos si sera un string, enumerador, int... Lo que el usuario quiera
+    private float score; // Que el usuario decida como de bueno es el caso una vez utilizado según sus propias métricas
+
     public CaseCBR() 
     { 
-
+        answer = null;
     }
     // Métodos para añadir a sus respectivas listas
-    #region addToCase
+    #region addToCase/setters
     public void addFloatToCase(float value, string name)
     {
         if(similarityFloat == null) similarityFloat = new Dictionary<string, float>();
@@ -72,10 +75,85 @@ public class CaseCBR
         if (similarityBool == null) similarityBool = new Dictionary<string, bool>();
         similarityBool[name] = value;
     }
-    public void addBoolToCase(List<bool> value, string name)
+    public void addBoolListToCase(List<bool> value, string name)
     {
         if (similarityBoolList == null) similarityBoolList = new Dictionary<string, List<bool>>();
         similarityBoolList[name] = value;
     }
+
+    public void setAnswer(dynamic value)
+    {
+        answer = value;
+    }
+    public void setScore(float value)
+    {
+        score = value;
+    }
+    #endregion
+
+    #region getters
+    public float getFloat(string name)
+    {
+        if (similarityFloat == null) throw new NullReferenceException("No existen elementos que mirar");
+        else if(!similarityFloat.ContainsKey(name)) throw new ArgumentOutOfRangeException("El argumento no existe");
+        else return similarityFloat[name];
+    }
+
+    public List<float> getFloatList(string name)
+    {
+        if (similarityFloatLists == null) throw new NullReferenceException("No existen elementos que mirar");
+        else if (!similarityFloatLists.ContainsKey(name)) throw new ArgumentOutOfRangeException("El argumento no existe");
+        else return similarityFloatLists[name];
+    }
+
+    public Tuple<DistanceMethod,UnityEngine.Vector2> getVector2(string name)
+    {
+        if (similarityVec2 == null) throw new NullReferenceException("No existen elementos que mirar");
+        else if (!similarityVec2.ContainsKey(name)) throw new ArgumentOutOfRangeException("El argumento no existe");
+        else return similarityVec2[name];
+    }
+
+    public Tuple<DistanceMethod, UnityEngine.Vector3> getVector3(string name)
+    {
+        if (similarityVec3 == null) throw new NullReferenceException("No existen elementos que mirar");
+        else if (!similarityVec3.ContainsKey(name)) throw new ArgumentOutOfRangeException("El argumento no existe");
+        else return similarityVec3[name];
+    }
+
+    public Tuple<DistanceMethod, List<UnityEngine.Vector2>> getVector2List(string name)
+    {
+        if (similarityVec2List == null) throw new NullReferenceException("No existen elementos que mirar");
+        else if (!similarityVec2List.ContainsKey(name)) throw new ArgumentOutOfRangeException("El argumento no existe");
+        else return similarityVec2List[name];
+    }
+    public Tuple<DistanceMethod, List<UnityEngine.Vector3>> getVector3List(string name)
+    {
+        if (similarityVec3List == null) throw new NullReferenceException("No existen elementos que mirar");
+        else if (!similarityVec3List.ContainsKey(name)) throw new ArgumentOutOfRangeException("El argumento no existe");
+        else return similarityVec3List[name];
+    }
+
+    public bool getBool(string name)
+    {
+        if (similarityBool == null) throw new NullReferenceException("No existen elementos que mirar");
+        else if (!similarityBool.ContainsKey(name)) throw new ArgumentOutOfRangeException("El argumento no existe");
+        else return similarityBool[name];
+    }
+    public bool getBoolList(string name)
+    {
+        if (similarityBool == null) throw new NullReferenceException("No existen elementos que mirar");
+        else if (!similarityBool.ContainsKey(name)) throw new ArgumentOutOfRangeException("El argumento no existe");
+        else return similarityBool[name];
+    }
+
+    public dynamic getAnswer()
+    {
+        return answer;
+    }
+    public float getScore()
+    {
+        return score;
+    }
+
     #endregion
 }
